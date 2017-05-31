a0a940af3816c40afacfa5f1ffe1f5e36fb59523
Guillaume Charmetant <cguille.dev@gmail.com>
2017-05-31 13:23:29 +0200

    use irc crate to read messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate irc;
+
+use std::default::Default;
+use irc::client::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let cfg = Config {
+        nickname: Some(String::from("cGuille-botcast")),
+        server: Some(String::from("irc.mozilla.org")),
+        channels: Some(vec![String::from("#rust-bots")]),
+        ..Default::default()
+    };
+
+    let server = IrcServer::from_config(cfg).unwrap();
+    server.identify().unwrap();
+
+    for message in server.iter() {
+        let message = message.unwrap();
+
+        match message.command {
+            Command::PRIVMSG(_, ref content) => println!("{}", content),
+            _ => (),
+        }
+    }
 }
